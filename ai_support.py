c19077b88106cdc4f67098e02df3ef1b2e04720c
Ananthu Jayakumar <115489088+ananthu666@users.noreply.github.com>
2023-09-12 01:01:06 +0530

    Update ai_support.py

--- ai_support.py
+++ ai_support.py
@@ -1,10 +1,13 @@
 import openai
 import requests
 import json
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
+api_key=os.getenv('api_key')
+
 def maveli(text):
-    api_key=os.getenv('api_key')
     if(api_key==''):
         return "Input API key"
     openai.api_key = api_key
